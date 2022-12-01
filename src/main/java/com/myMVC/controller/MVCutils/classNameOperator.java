4f445ca3d051af01445209c87a4a437600c1e049
bcq <3488613264@qq.com>
2022-12-01 08:58:24 +0800

    finish misson1 & 2

---
+++ src/main/java/com/myMVC/controller/MVCutils/classNameOperator.java
@@ -0,0 +1,17 @@
+package com.myMVC.controller.MVCutils;
+
+public class classNameOperator {
+    /**
+     * 将 class或interface A.B.C.D 转化为A.B.C.D
+     * @return
+     */
+    public static String getClassName(String type){
+        StringBuffer stringBuffer = new StringBuffer(type);
+        while(stringBuffer.charAt(0)!=' '){
+            stringBuffer.deleteCharAt(0);
+        }
+        //去空格
+        stringBuffer.deleteCharAt(0);
+        return stringBuffer.toString();
+    }
+}
